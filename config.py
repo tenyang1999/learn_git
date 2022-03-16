b29fbcb59580dd5146ad6d639829901c2292feea
TenYang <tzuhsuan1010@gmail.com>
2022-03-16 17:02:54 +0800

    Changes to be committed:

---
+++ config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 13:45:23 2022
+
+@author: TenYang
+"""
+create_data_config={
+    'name' : "company_name",
+    'task' : 'rtime,inbound_utility',
+    'time' : '5min'}
+
+total_config ={
+    'look_back' : 12,
+    'epochs' : 50,
+    'batch_size' : 2,
+    'verbose': 0,
+    "path":'C:/Users/Administrator/static_seq2seq'
+          }
+    
